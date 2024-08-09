17af6ca5142cf205e1c421cb1813ff293b8b9e64
sergeytsvir <tsvir36@gmail.com>
2024-08-09 11:12:54 +0300

    feat: add solution morse

--- src/index.js
+++ src/index.js
@@ -39,8 +39,23 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let array = expr.split('');
+   let arraySize = 10
+   let arrayNew = [];
+  for(let i = 0; i < array.length; i += arraySize){
+    arrayNew.push(array.slice(i, i + arraySize))
+  }
+  let res = arrayNew.map((e) => e.join('')).map((e) => e.replaceAll('10','.').replaceAll('11','-').replaceAll('00',''));
+  let str = '';
+  res.forEach((e) => {
+    if(MORSE_TABLE[e] !== undefined) str += MORSE_TABLE[e];
+    else if (MORSE_TABLE[e] === undefined) str += ' ';
+  })
+  return str
 }
 
 module.exports = {
     decode
 }
+
+console.log(decode('hello word'))
